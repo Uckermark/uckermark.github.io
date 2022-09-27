e527457cb31e11ed5318211269d8a03697b2692e
Uckermark <llausen@protonmail.com>
2022-09-27 20:39:54 +0200

    x

--- repo/package_reload.py
+++ repo/package_reload.py
@@ -1,12 +1,10 @@
 import os
 
-os.system('dpkg-scanpackages ./Files > Packages')
+os.system('dpkg-scanpackages ./debs > Packages')
 
 with open("Packages", 'r') as f:
 	packages =  f.read()
 packages = packages.replace("iphoneos-arm", "iphoneos-arm64")
-packages = packages.replace("./", "https://uckermark.github.io/repo/")
-# print(var)
 os.system('rm Packages')
 os.system('touch Packages')
 with open("Packages", 'w') as f:
