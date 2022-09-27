33fa06bc8f9881f1f822c6f9fce444d74f7cbc12
Uckermark <llausen@protonmail.com>
2022-09-27 23:53:59 +0200

    another arm64 patch

--- repo/package_reload.py
+++ repo/package_reload.py
@@ -4,8 +4,8 @@ os.system('dpkg-scanpackages ./debs > Packages')
 
 with open("Packages", 'r') as f:
 	packages =  f.read()
-packages = packages.replace("iphoneos-arm", "iphoneos-arm64")
-packages = packages.replace("iphoneos-arm6464", "iphoneos-arm64")
+packages = packages.replace(" iphoneos-arm", " iphoneos-arm64")
+# packages = packages.replace("iphoneos-arm6464", "iphoneos-arm64")
 os.system('rm Packages')
 os.system('touch Packages')
 with open("Packages", 'w') as f:
