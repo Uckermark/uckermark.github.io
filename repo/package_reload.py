7f9eec03f700849e691d46bc0743152950a30543
Uckermark <93996544+Uckermark@users.noreply.github.com>
2022-09-28 22:07:17 +0200

    no longer needed

--- repo/package_reload.py
+++
@@ -1,7 +0,0 @@
-import os
-os.system('dpkg-scanpackages ./debs > Packages')
-os.system('rm Packages.gz')
-os.system('rm Packages.bz2')
-os.system('gzip -c9 Packages > Packages.gz')
-os.system('bzip2 -c9 Packages > Packages.bz2')
-print("finished!")
