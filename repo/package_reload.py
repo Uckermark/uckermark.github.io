7937b971ad8d064e7dcd9895aceff70365dd1166
Uckermark <93996544+Uckermark@users.noreply.github.com>
2022-09-28 22:04:30 +0200

    removed iphoneos-arm64 stuff
    was no longer needed

--- repo/package_reload.py
+++ repo/package_reload.py
@@ -1,19 +1,7 @@
 import os
 os.system('dpkg-scanpackages ./debs > Packages')
-
-with open("Packages", 'r') as f:
-	packages =  f.read()
-packages = packages.replace(" iphoneos-arm", " iphoneos-arm64")
-packages = packages.replace("iphoneos-arm6464", "iphoneos-arm64")
-os.system('rm Packages')
-os.system('touch Packages')
-with open("Packages", 'w') as f:
-	f.write(packages)
 os.system('rm Packages.gz')
 os.system('rm Packages.bz2')
 os.system('gzip -c9 Packages > Packages.gz')
 os.system('bzip2 -c9 Packages > Packages.bz2')
 print("finished!")
-
-# TODO: new debs have to change arch to iphoneos-arm64
-# deb/control change architecture
