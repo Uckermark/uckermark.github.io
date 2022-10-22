2b43007100829553b7876c68e11d26ef3b1353e2
Uckermark <93996544+Uckermark@users.noreply.github.com>
2022-10-22 23:10:58 +0200

    Update weedra1n.sh

--- weedra1n.sh
+++ weedra1n.sh
@@ -1,3 +1,2 @@
 #!/var/jb/bin/sh
-
-/var/jb/usr/bin/echo "Types: deb\nURIs: https://mineek.github.io/repo/\nSuites: ./\nComponents:\n" > /var/jb/etc/apt/sources.list.d/procursus.sources
+printf "Types: deb\nURIs: https://mineek.github.io/repo/\nSuites: ./\nComponents:\n" > /var/jb/etc/apt/sources.list.d/procursus.sources
