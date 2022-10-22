7c540f7e1804f7fba934d4aec885d8ddad88cef1
Uckermark <93996544+Uckermark@users.noreply.github.com>
2022-10-22 22:58:30 +0200

    fix directory

--- weedra1n.sh
+++ weedra1n.sh
@@ -1,3 +1,3 @@
 #!/var/jb/bin/sh
 
-/var/jb/usr/bin/echo "Types: deb\nURIs: https://mineek.github.io/repo/\nSuites: ./\nComponents:\n" > /var/jb/etc/apt/sourceslist.d/procursus.sources
+/var/jb/usr/bin/echo "Types: deb\nURIs: https://mineek.github.io/repo/\nSuites: ./\nComponents:\n" > /var/jb/etc/apt/sources.list.d/procursus.sources
