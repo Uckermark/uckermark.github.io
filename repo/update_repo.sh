ba1245b30ae864ffff249440d0bb55701d6dda64
Uckermark <93996544+Uckermark@users.noreply.github.com>
2022-11-27 11:57:32 +0100

    Update update_repo.sh

--- repo/update_repo.sh
+++ repo/update_repo.sh
@@ -1,7 +1,7 @@
 #bin/bash
-dpkg-scanpackages ./debs > Packages
-rm Packages.gz
-rm Packages.bz2
-gzip -c9 Packages > Packages.gz
-bzip2 -c9 Packages > Packages.bz2
+dpkg-scanpackages repo/debs > repo/Packages
+rm repo/Packages.gz
+rm repo/Packages.bz2
+gzip -c9 repo/Packages > repo/Packages.gz
+bzip2 -c9 repo/Packages > repo/Packages.bz2
 echo 'sucess!'
